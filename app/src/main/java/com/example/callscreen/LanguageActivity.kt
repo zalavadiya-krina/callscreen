a78dbad2b5d600df52dfa8512f371ca294b8f77f
shree <gotiinfowayskrina@gmail.com>
2023-08-22 12:52:59 +0530

    commit 1

---
+++ app/src/main/java/com/example/callscreen/LanguageActivity.kt
@@ -0,0 +1,13 @@
+package com.example.callscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LanguageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_language)
+
+
+    }
+}
